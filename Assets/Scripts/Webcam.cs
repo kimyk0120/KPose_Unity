a76790c4f887dbcf0c1b9eb72ca75f66da74b39b
kimyk0120 <kimyk0120@hotmail.com>
2019-09-24 14:47:12 +0900

    init

---
+++ Assets/Scripts/Webcam.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Webcam : MonoBehaviour
+{
+    static WebCamTexture backCam;
+    public RawImage rawImage;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(backCam == null)
+            backCam = new WebCamTexture();        
+        rawImage.texture = backCam;
+        rawImage.material.mainTexture = backCam;
+        if(!backCam.isPlaying) backCam.Play();    
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# KPose_Unity
